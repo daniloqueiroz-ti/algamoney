f095c18033e02ff644a4b218aa0361671d06f1f1
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-06-07 20:44:35 -0300

    Update security to jwt token

--- algamoney-backend/algamoney-api/src/main/java/br/com/queiroz/config/BasicSecurityConfigInMemory.java
+++ algamoney-backend/algamoney-api/src/main/java/br/com/queiroz/config/BasicSecurityConfigInMemory.java
@@ -9,38 +9,39 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/*Basic Security Config*/
 //@Configuration
 //@EnableWebSecurity
-public class BasicSecurityConfigInMemory extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication().withUser("admin").password("$2a$10$L44rTH7Kr4fJKHofolR6OushTu9uYykeSLitNDIcm.QpN1K2oP862").roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
-	}
-	
-	//gerar senha codificada
-//	public static void main(String[] args) {
-//		System.out.print(new BCryptPasswordEncoder().encode("admin"));
-//		//$2a$10$L44rTH7Kr4fJKHofolR6OushTu9uYykeSLitNDIcm.QpN1K2oP862
+//public class BasicSecurityConfigInMemory extends WebSecurityConfigurerAdapter {
+//
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+////		auth.inMemoryAuthentication().withUser("admin").password("$2a$10$L44rTH7Kr4fJKHofolR6OushTu9uYykeSLitNDIcm.QpN1K2oP862").roles("ADMIN");
+//		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
 //	}
-	
-//	@Bean
-//	public BCryptPasswordEncoder passwordEncoder() {
-//	    return new BCryptPasswordEncoder();
+//	
+//	//gerar senha codificada
+////	public static void main(String[] args) {
+////		System.out.print(new BCryptPasswordEncoder().encode("admin"));
+////		//$2a$10$L44rTH7Kr4fJKHofolR6OushTu9uYykeSLitNDIcm.QpN1K2oP862
+////	}
+//	
+////	@Bean
+////	public BCryptPasswordEncoder passwordEncoder() {
+////	    return new BCryptPasswordEncoder();
+////	}
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//			.antMatchers("/categorias").permitAll()
+//			.anyRequest().authenticated()
+//			.and()
+//			.httpBasic()
+//			.and()
+//			.sessionManagement()
+//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//			.and()
+//			.csrf().disable();
 //	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/categorias").permitAll()
-			.anyRequest().authenticated()
-			.and()
-			.httpBasic()
-			.and()
-			.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.csrf().disable();
-	}
-}
+//}
