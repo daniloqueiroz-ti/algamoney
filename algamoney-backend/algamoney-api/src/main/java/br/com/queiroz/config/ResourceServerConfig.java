5565b10425c671040465659fa0f4ef3503b2eebb
danilo.queiroz <danilo.queiroz@crptecnologia.com.br>
2021-06-10 19:00:48 -0300

    update security to userDetailsService

--- algamoney-backend/algamoney-api/src/main/java/br/com/queiroz/config/ResourceServerConfig.java
+++ algamoney-backend/algamoney-api/src/main/java/br/com/queiroz/config/ResourceServerConfig.java
@@ -1,34 +1,29 @@
 package br.com.queiroz.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
-@EnableWebSecurity
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	@Autowired
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		//auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("admin").password("$2a$10$L44rTH7Kr4fJKHofolR6OushTu9uYykeSLitNDIcm.QpN1K2oP862").roles("ADMIN");
-	}
-
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/categorias").permitAll().anyRequest().authenticated().and()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/categorias").permitAll()
+		.anyRequest().authenticated()
+		.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.csrf().disable();
 	}
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.stateless(true);
 	}
+	
 }
